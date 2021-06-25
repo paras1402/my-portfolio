85cf031b7befa15148c76cc67fbb62bf007a779b
paras <paraskangra14@gmail.com>
2021-06-25 13:18:09 +0530

    intro done

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,9 +1,36 @@
 import "./Intro.scss";
-
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 const Intro = () => {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Use with React.js!", "Yeah!"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>My Name</h1>
+          <h3>
+            FreeLance <span ref={textRef}>Designer</span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 };
